8746a6ba1befdc1012ec144efd6e5f2fd46c77f0
Ali <behzad.ali@capgemini.com>
2023-12-15 20:10:04 +0530

    Day1 - Variables

---
+++ src/com/wintertraining/day1/Variables.java
@@ -0,0 +1,18 @@
+package com.wintertraining.day1;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int age = 1;
+		String name = "Aa";
+		double salary = 3.402823; 
+		boolean active = false;
+		
+		
+		System.out.println(age+"\n"+name+"\n"+salary+"\n"+active);
+		
+		
+		System.out.println(name.hashCode());
+	}
+}
